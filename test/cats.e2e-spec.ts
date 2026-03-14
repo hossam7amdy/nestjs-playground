fcac40a1717a382f051b12a6e2ebad70f723cc58
Hossam Hamdy <hossamhamdy117@gmail.com>
2026-03-14 23:11:51 +0200

    add end-to-end tests for Cats module

--- test/cats.e2e-spec.ts
+++ test/cats.e2e-spec.ts
@@ -2,24 +2,33 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import { CatsModule } from '../src/cats/cats.module';
+import { CatsService } from '../src/cats/cats.service';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
+  const catsService = { findAll: () => [] };
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+      imports: [CatsModule],
+    })
+      .overrideProvider(CatsService)
+      .useValue(catsService)
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('/GET cats', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/cats')
       .expect(200)
-      .expect('Hello World!');
+      .expect(catsService.findAll());
   });
 });
