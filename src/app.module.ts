d01126a3d61a752acd4d99a8604ae852231464ad
Hossam Hamdy <hossamhamdy117@gmail.com>
2026-03-14 21:36:34 +0200

    implement initial NestJS application structure with AppController, AppService, and lifecycle event handling

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,38 @@
 import { Module } from '@nestjs/common';
+import type {
+  OnModuleInit,
+  OnModuleDestroy,
+  OnApplicationShutdown,
+  BeforeApplicationShutdown,
+} from '@nestjs/common';
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule
+  implements
+    OnModuleInit,
+    OnModuleDestroy,
+    OnApplicationShutdown,
+    BeforeApplicationShutdown
+{
+  onModuleInit() {
+    console.log('onModuleInit');
+  }
+
+  onModuleDestroy() {
+    console.log('onModuleDestroy');
+  }
+
+  beforeApplicationShutdown(signal?: string) {
+    console.log('beforeApplicationShutdown', signal);
+  }
+
+  onApplicationShutdown(signal?: string) {
+    console.log('onApplicationShutdown', signal);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const port = process.env.PORT ?? 3000;
+  const loggerMessage = `Application is running on: http://localhost:${port}`;
+
+  const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // consumes memory by starting listeners.
+  app.enableShutdownHooks();
+
+  await app.listen(port);
+  logger.log(loggerMessage);
+
+  let counter = 0;
+  setInterval(() => {
+    // still runs even when the app stops
+    console.log('counter:', counter++);
+  }, 1000);
+
+  await app.close();
 }
 void bootstrap();
