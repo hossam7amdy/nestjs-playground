4c597af9ea5c01b74e20448900a23167cea82fe5
Hossam Hamdy <hossam.hamdy@atomica.ai>
2026-03-14 14:22:56 +0200

    feat: add TransientService and implement circular dependency resolution in AppService

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { CommonModule } from './common/common.module';
 import { CommonService } from './common/common.service';
+import { TransientService } from './transient.service';
 
 @Module({
   imports: [CommonModule],
   controllers: [],
-  providers: [AppService, CommonService],
+  providers: [AppService, CommonService, TransientService],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { CommonService } from './common/common.service';
-import { ModuleRef } from '@nestjs/core';
+import { ContextIdFactory, ModuleRef } from '@nestjs/core';
+import { TransientService } from './transient.service';
+import assert from 'node:assert';
 
 @Injectable()
 export class AppService implements OnModuleInit {
@@ -8,7 +10,30 @@ export class AppService implements OnModuleInit {
 
   constructor(private moduleRef: ModuleRef) {}
 
-  onModuleInit() {
+  async onModuleInit() {
     this.commonService = this.moduleRef.get(CommonService);
+
+    await this.resolveTransientScopedProviders();
+    await this.resolveSharedTransientScopedProviders();
+  }
+
+  async resolveTransientScopedProviders() {
+    const transientServices = await Promise.all([
+      this.moduleRef.resolve(TransientService),
+      this.moduleRef.resolve(TransientService),
+    ]);
+
+    assert(transientServices[0] !== transientServices[1]);
+  }
+
+  async resolveSharedTransientScopedProviders() {
+    const contextId = ContextIdFactory.create();
+
+    const transientServices = await Promise.all([
+      this.moduleRef.resolve(TransientService, contextId),
+      this.moduleRef.resolve(TransientService, contextId),
+    ]);
+
+    assert(transientServices[0] === transientServices[1]);
   }
 }
