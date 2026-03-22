d2e8e2a722f7c21c1b72620ded25e92771e4e7ae
Hossam Hamdy <hossamhamdy117@gmail.com>
2026-03-22 15:35:47 +0200

    feat: add CreateCatDto with validation for creating a cat entity.

--- src/cats/dto/create-cat.dto.ts
+++ src/cats/dto/create-cat.dto.ts
@@ -1,1 +1,13 @@
-export class CreateCatDto {}
+import { IsString, IsInt, Min } from 'class-validator';
+
+export class CreateCatDto {
+  @IsString()
+  name: string;
+
+  @IsInt()
+  @Min(0)
+  age: number;
+
+  @IsString()
+  breed: string;
+}
